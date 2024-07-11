fed519409f17fb4012bbc21e6ec8543e312dec71
draz <draz.primas@gmail.com>
2024-07-11 14:25:27 +0200

    white outline

--- main.c
+++ main.c
@@ -29,7 +29,14 @@ int main(int argc, char **argv) {
 
     int *arr = malloc((dim/2)*sizeof(int));
 
+
+    for (int i = 0; i < dim+2 - dim%2; ++i)
+        printf("%s  ", resetstr);
+    printf("\e[0m\n");
+
     for (int row = 0; row < dim; ++row) {
+
+        printf("%s  ", resetstr);
         for (int i = 0; i < dim/2; ++i) {
             arr[i] = rnd()%2;
             printf("%s  ", arr[i] ? colourstr : resetstr);
@@ -38,8 +45,13 @@ int main(int argc, char **argv) {
         for (int i = 0; i < dim/2; ++i)
             printf("%s  ", arr[dim/2 - i - 1] ? colourstr : resetstr);
 
+        printf("%s  ", resetstr);
         printf("\e[0m\n");
     }
+
+    for (int i = 0; i < dim+2 - dim%2; ++i)
+        printf("%s  ", resetstr);
+    printf("\e[0m\n");
 
     free(arr);
     fclose(rndfile);
